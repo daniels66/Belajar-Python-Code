414c08f8e34c99ff52ac05976094c53cd3824b46
daniels66 <danielsaputra814@gmail.com>
2022-08-22 11:45:15 +0700

    cara compile file python seperti bahasa cpp, java

---
+++ 01-Compile Pyhton/Main.py
@@ -0,0 +1,13 @@
+# meskipun bahasa python merupakan bahasa yang interpreter
+# kita bisa membuatnya menjadi bahasa compile seperti java ,c, cpp, dll
+# yaitu dengan cara menuliskan 'python3 -m py_compile Main.py'
+# yang akan dibuatkan folder '__pycache__'
+# jika akan menjalankan python yang dicompile pindah ke folder __pycache__
+# dan lakukan run seperti di phyton 'python3 Main.cpython-310.pyc'
+
+# keuntungan dalam menjalankan python yan telah di compile terletak di waktu
+# pemrosesan program akan semakin cepat
+
+print("hello world")
+msg = "hello world"
+print(msg)
