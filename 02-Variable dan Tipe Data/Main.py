b6aae8544250c70da7f49b148325d18cf5021924
daniels66 <danielsaputra814@gmail.com>
2022-08-22 14:26:16 +0700

    variable dan tipe data dalam python

---
+++ 02-Variable dan Tipe Data/Main.py
@@ -0,0 +1,26 @@
+# membuat variabel dalam bahasa python
+# dalam python tidak memerlukan penulisan type data (python otomatis mengetahui)
+# serta tidak perlu untuk mendeklarasikan variable | int a = 10
+nilai_a = 1
+print("nilai a adalah =", nilai_a,"\n")
+
+# type data dalam python
+# - integer (bilangan bulat)
+type_int = 12
+print("nilai variable adalah", type_int)
+print("type variable adalah",type(type_int),"\n")
+
+# - float (bilangan desimal / bilangan koma)
+type_float = 3.14
+print("nilai variable adalah", type_float)
+print("type variable adalah",type(type_float),"\n")
+
+# - string (kata)
+type_string = "daniels"
+print("nilai variable adalah", type_string)
+print("type variable adalah",type(type_string),"\n")
+
+# - boolean (True / False)
+type_bool = True
+print("nilai variable adalah", type_bool)
+print("type variable adalah",type(type_bool),"\n")
