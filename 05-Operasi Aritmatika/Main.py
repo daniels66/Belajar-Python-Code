d2b48dd09593ba1ba6d911af5944c7767d3094b9
daniels66 <danielsaputra814@gmail.com>
2022-08-24 18:22:36 +0700

    membuat folder operasi aritmatika

---
+++ 05-Operasi Aritmatika/Main.py
@@ -0,0 +1,45 @@
+'''
+Operasi aritmatika dalam python terdapat urutan operasi
+1. () Tanda Kurung
+2. ** Pemangkatan
+3. * / % Perkalian, Pembagian, Modulus
+4. + - Tambah, Kurang
+'''
+
+a = 10
+b = 2
+
+# Operasi Penjumlahan (+)
+hasil = a + b
+print("Penjumlahan (+)")
+print(a,'+',b,'=',hasil,'\n')
+
+# Operasi Pengurangan (-)
+hasil = a - b
+print("Pengurangan (-)")
+print(a,'-',b,'=',hasil,'\n')
+
+# Operasi Perkalian (*)
+hasil = a * b
+print("Perkalian (*)")
+print(a,'*',b,'=',hasil,'\n')
+
+# Operasi Pembagian (/)
+hasil = a / b
+print("Pembagian (/)")
+print(a,'/',b,'=',hasil,'\n')
+
+# Operasi Modulus (%)
+hasil = a % b
+print("Modulus (%)")
+print(a,'%',b,'=',hasil,'\n')
+
+# Operasi Floor Division (//)
+hasil = a // b
+print("Floor Division (//)")
+print(a,'//',b,'=',hasil,'\n')
+
+# Operasi Pangkat (**)
+hasil = a ** b
+print("Pangkat (**)")
+print(a,'**',b,'=',hasil,'\n')
