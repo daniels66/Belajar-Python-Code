0d7ae94244c09fa83ff6c3a6f6ffbd21e7659357
daniels66 <danielsaputra814@gmail.com>
2022-09-15 15:56:39 +0700

    update sort,search array

--- NumPy 08-Sort, Search  Array di NumPy/main.py
+++ NumPy 08-Sort, Search  Array di NumPy/main.py
@@ -19,7 +19,14 @@ print(f"nilai setelah diurutkan dari a = \n{np.sort(a)}")
 """
 untuk sort diurutkan pada masing masing kolom dengan 
 nilai terbesar berada disebelah kanan,
-nilai terkecil berada di sebelah kiri
+nilai terkecil berada di sebelah kiri,
+
+apabila mengurutkan string maka,
+abjad awal akan berada di sebelah kiri,
+abjad akhir akan berada di sebelah kanan,
+
+apabila mengurutkan boolean maka,
+false akan didahulukan, kemudian true
 """
 print(f"nilai setelah diurutkan dari a = \n{np.argsort(a)}")
 
@@ -37,3 +44,25 @@ print(f"sort dengan tinggi badan")
 print(np.sort(h, order="tinggi_bdn"))
 print(f"sort dengan nama")
 print(np.sort(h, order="nama"))
+
+# mencari nilai
+# menggunakan where() dan searchsort()
+print("search nilai".center(30, "-"))
+b = np.array([1, 2, 3, 4, 5, 7, 8, 9, 8, 7, 7])
+print(b)
+
+# where(array == <apa yang dicari>)
+b_where = np.where(
+    b == 7
+)  # akan menghasilkan semua index dimana nilai tersebut disimpan dalam array
+print(b_where)
+b_where = np.where(b == 6)  # tidak akan ada nilai index yang ditampilkan dalam array
+print(b_where)
+
+# searchsorted(array , nilai, sisi)
+b_srcst = np.searchsorted(
+    b, 7
+)  # akan menghasilkan index awal dimana nilai tersebut disimpan dalam array
+print("nilai 7 berada pada index ", b_srcst, "dari kiri")
+b_srcst = np.searchsorted(b, 7, side="right")
+print("nilai 7 berada pada index ", b_srcst, "dari kanan")
