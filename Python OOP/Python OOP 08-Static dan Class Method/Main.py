eb59b1e11c401338aa9904030a306fb7e404b5cc
daniels66 <danielsaputra814@gmail.com>
2022-09-20 20:03:33 +0700

    static dan class method

--- Python OOP/Python OOP 08-Static dan Class Method/Main.py
+++ Python OOP/Python OOP 08-Static dan Class Method/Main.py
@@ -1,34 +1,58 @@
 # static dan class method dalam sebuah class
 class Hero:
-
-    # private class variabel
     __jumlah = 0
 
     def __init__(self, name):
         self.__name = name
         Hero.__jumlah += 1
+        print("hero" + str(Hero.__jumlah) + " adalah =", self.__name)
 
-    # method ini hanya berlaku untuk objek
-    def getJumlah(self):
+    # hanya berlaku untuk objek
+    def getHero1(self):
         return Hero.__jumlah
 
-    # method ini tidak berlaku untuk objek tapi berlaku untuk class
-    def getJumlah1():
+    # tidak berlaku untuk objek, berlaku untuk class
+    def getHero2():
         return Hero.__jumlah
 
-    # method static (decorator) nempel ke objek dan class
+    # static method (decorator)
+    # berlaku untuk objek dan class
     @staticmethod
-    def getJumlah2():
+    def getHero():
         return Hero.__jumlah
 
     @classmethod
-    def getJumlah3(cls):
+    def getHero3(cls):
         return cls.__jumlah
 
 
-sniper = Hero("sniper")
-print(Hero.getJumlah2())
-rikimaru = Hero("rikimaru")
-print(sniper.getJumlah2())
-drowranger = Hero("drowranger")
-print(Hero.getJumlah3())
+hero1 = Hero("daniel")
+# print(Hero.__jumlah) # akan eror apabila ingin mengambil variable private diluar class
+# apabila menggunakan getter
+# print(Hero.getHero1()) # akan eror dikarenakan hanya berlaku untuk objek
+print(hero1.getHero1())  # getHero1() berlaku untuk objek
+print(Hero.getHero2())  # getHero2() berlaku untuk class
+# print(hero1.getHero2())  # akan eror dikarenakan hanya berlaku untuk class
+"""
+dapatkah mengambil getHero() yang berlaku untuk objek dan class??
+bisa dengan menggunakan static method
+"""
+print(hero1.getHero())  # tidak akan eror dikarenakan static method
+print(Hero.getHero())  # tidak akan eror dikarenakan static method
+"""
+selain static method ada juga class method yang berguna pada saat merubah nama class
+    @staticmethod
+    def getHero():
+        return Hero.__jumlah
+di static method apabila ingin merubah nama class maka nama class sebelumnya (Hero)
+harus ikut dirubah, apabila tidak banyak tidak apa-apa, kalau banyak??
+oleh karena itu menggunakan class method
+    @classmethod
+    def getHero(cls):
+        return cls.__jumlah
+    *cls dapat ditulis terserah
+"""
+hero2 = Hero("bili")
+print(hero1.getHero3())
+hero3 = Hero("okti")
+print(Hero.getHero3())
