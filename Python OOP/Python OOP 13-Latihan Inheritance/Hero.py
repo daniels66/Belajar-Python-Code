5d640cc6c0e97194914f54988400dc7b2ba53215
daniels66 <danielsaputra814@gmail.com>
2022-09-21 15:38:19 +0700

    update latihan inheritance

--- Python OOP/Python OOP 13-Latihan Inheritance/Hero.py
+++ Python OOP/Python OOP 13-Latihan Inheritance/Hero.py
@@ -1,16 +1,21 @@
 class Hero:
     def __init__(self, name):
-        self.health_pool = [0, 100, 200, 300, 400, 500]
-        self.attPower_pool = [0, 10, 20, 30, 40, 50]
-        self.armor_pool = [0, 1, 2, 3, 4, 5]
+        self.health_pool = [0, 100, 200, 300, 400, 500]  # set health default
+        self.attPower_pool = [0, 10, 20, 30, 40, 50]  # set attack power default
+        self.armor_pool = [0, 1, 2, 3, 4, 5]  # set armor default
         self.__name = name
         self.__exp = 0
         self.__level = 0
 
     def show_info(self):
         print(
-            "{} \n\tlevel: {}\n\thealth: {} \n\tattPower: {} \n\tarmor: {}".format(
-                self.__name, self.__level, self.__health, self.__attPower, self.__armor
+            "{} \n\tlevel: {}\n\thealth: {} \n\tattPower: {} \n\tarmor: {} \n\texp: {}".format(
+                self.__name,
+                self.__level,
+                self.__health,
+                self.__attPower,
+                self.__armor,
+                self.__exp,
             )
         )
 
@@ -50,15 +55,21 @@ class Hero:
     def gainExp(self, input):
         self.__exp += input
         if self.__exp >= 100:
-            self.levelUp = self.__exp // 100
+            self.levelUp = self.__exp // 100  # lina exp +200 (200//100=2)
             self.__exp %= 100
 
     @levelUp.setter
     def levelUp(self, input):
-        self.__level += input
-        self.__health = self.__health_pool[self.__level]
-        self.__attPower = self.__attPower_pool[self.__level]
-        self.__armor = self.__armor_pool[self.__level]
+        self.__level += input  # (1+2=2) nilai 1 dari awal nilai 2 dari gainEXp
+        self.__health = self.__health_pool[
+            self.__level
+        ]  # mengambil array self.__health_pool dengan index 3 (refer ke class turunan)
+        self.__attPower = self.__attPower_pool[
+            self.__level
+        ]  # mengambil array self.__attPower_pool dengan index 3 (refer ke class turunan)
+        self.__armor = self.__armor_pool[
+            self.__level
+        ]  # mengambil array self.__armor_pool dengan index 3 (refer ke class turunan)
 
 
 class HeroIntelligent(Hero):
@@ -67,7 +78,7 @@ class HeroIntelligent(Hero):
         self.health_pool = [0, 50, 100, 150, 200, 250]
         self.attPower_pool = [0, 20, 40, 60, 80, 100]
         self.armor_pool = [0, 0.5, 1, 1.5, 2, 2.5]
-        self.levelUp = 1
+        self.levelUp = 1  # input pada levelUp menjadi 1
 
 
 class HeroStrength(Hero):
--- Python OOP/Python OOP 13-Latihan Inheritance/Main.py
+++ Python OOP/Python OOP 13-Latihan Inheritance/Main.py
@@ -6,9 +6,22 @@ slardar = HeroStrength("slardar")
 
 lina.show_info()
 slardar.show_info()
+print(30 * "-")
 
-lina.gainExp = 200
-slardar.gainExp = 250
-
+lina.gainExp = 50
+slardar.gainExp = 75
 lina.show_info()
 slardar.show_info()
+print(30 * "-")
+
+lina.gainExp = 100
+slardar.gainExp = 75
+lina.show_info()
+slardar.show_info()
+print(30 * "-")
+
+lina.gainExp = 50
+slardar.gainExp = 75
+lina.show_info()
+slardar.show_info()
+print(30 * "-")
