7a5ce4b2d5bf1c08c9faef809a275a529567e784
daniels66 <danielsaputra814@gmail.com>
2022-09-26 15:18:58 +0700

    menambahkan judul dan icon dalam pygame

--- Pygame/Pygame/Pygame 02-Judul dan Icon/main.py
+++ Pygame/Pygame/Pygame 02-Judul dan Icon/main.py
@@ -4,13 +4,17 @@ import pygame, sys
 pygame.init()
 
 # untuk mengatur lebar layar pygame
-lebar = 400
-tinggi = 500
+lebar = 800
+tinggi = 600
 screen = pygame.display.set_mode(
     (lebar, tinggi)
 )  # akan flash (buka dan tutup game dalam satu waktu)
 
-#
+# mengatur judul dan icon pygame
+pygame.display.set_caption("Space Invader")  # judul game
+icon = pygame.image.load("gambar/ufo.png")  # letak icon
+pygame.display.set_icon(icon)  # tampilkan icon
+
 # looping game
 while True:
     for event in pygame.event.get():
